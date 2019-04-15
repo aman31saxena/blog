0061cf0c39879dd934fa996762526e8f70679809
Aman Saxena <aman31saxena@gmail.com>
2019-04-15 08:52:26 +0530

    Updated urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,12 @@ from . import views
 from django.conf.urls import url
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-	url(r'^home/$', views.home, name='home'),
+	path('home/', views.home, name='home'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 	path('post/new', views.post_new, name='post_new'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    url(r'^login/$', views.login, name='login'),
+    path('login/', views.login, name='login'),
 	path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     #url(r'^logout/$', views.logout, name='logout'),	
-    url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
+    path('auth/', include('social_django.urls', namespace='social')),  # <- Here
 ]
